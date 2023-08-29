f55a984e50af89e9e42166f6f91a849a7eb09f6e
Jonathan Mitchell <jonmitchell1234@gmail.com>
2023-08-29 14:08:26 -0400

    fixed routing issue on cards by adding link from react router dom

--- src/Components/about.js
+++ src/Components/about.js
@@ -1,28 +1,28 @@
-import * as React from 'react';
-import Card from '@mui/material/Card';
-import CardActions from '@mui/material/CardActions';
-import CardContent from '@mui/material/CardContent';
-import CardMedia from '@mui/material/CardMedia';
-import Button from '@mui/material/Button';
-import Typography from '@mui/material/Typography';
+import * as React from "react";
+import Card from "@mui/material/Card";
+import CardActions from "@mui/material/CardActions";
+import CardContent from "@mui/material/CardContent";
+import CardMedia from "@mui/material/CardMedia";
+import Button from "@mui/material/Button";
+import Typography from "@mui/material/Typography";
 import styled from "styled-components";
+import { Link } from "react-router-dom"; // Import the Link component
 
 const CardContainer = styled.div`
-    display: flex;
-    flex-direction: row;
-    justify-content: space-around;
-    align-items: center;
-    margin: 0 auto;
-    width: 100%;
+	display: flex;
+	flex-direction: row;
+	justify-content: space-around;
+	align-items: center;
+	margin: 0 auto;
+	width: 100%;
 
-    @media (max-width: 768px) {
-        flex-direction: column;
-    };
-    `;
-
+	@media (max-width: 768px) {
+		flex-direction: column;
+	}
+`;
 
 export default function MediaCard() {
-  return (
+	return (
 		<>
 			<CardContainer>
 				<Card sx={{ maxWidth: 545, border: "2px solid white" }}>
@@ -36,12 +36,15 @@ export default function MediaCard() {
 							Cats
 						</Typography>
 						<Typography variant="body2" color="text.secondary">
-							Cats were first domesticated around 7500 BCE in the western Asia region and are currently the second most popular domestic pet in the United States. Click learn more below to find out more about caring for your cat.
+							Cats were first domesticated around 7500 BCE in the western Asia region and are currently the second most popular domestic
+							pet in the United States. Click learn more below to find out more about caring for your cat.
 						</Typography>
 					</CardContent>
 					<CardActions>
 						<Button size="small">Share</Button>
-						<Button size="small"><a href='views/cats'>Learn More</a></Button>
+						<Button size="small">
+							<Link to="/views/cats">Learn More</Link> {/* Use Link component */}
+						</Button>
 					</CardActions>
 				</Card>
 				<Card sx={{ maxWidth: 545, border: "2px solid white" }}>
@@ -55,12 +58,15 @@ export default function MediaCard() {
 							Dogs
 						</Typography>
 						<Typography variant="body2" color="text.secondary">
-							The domestic dog is an extremely social animal and offers a diverse variety of choices as there are well over 300 breeds recognized by the World Canine Organization. Click learn more below to find out more about caring for your dog.
+							The domestic dog is an extremely social animal and offers a diverse variety of choices as there are well over 300 breeds
+							recognized by the World Canine Organization. Click learn more below to find out more about caring for your dog.
 						</Typography>
 					</CardContent>
 					<CardActions>
 						<Button size="small">Share</Button>
-						<Button size="small"><a href='views/dogs'>Learn More</a></Button>
+						<Button size="small">
+							<Link to="/views/dogs">Learn More</Link> {/* Use Link component */}
+						</Button>
 					</CardActions>
 				</Card>
 				<Card sx={{ maxWidth: 545, border: "2px solid white" }}>
@@ -74,15 +80,19 @@ export default function MediaCard() {
 							Birds
 						</Typography>
 						<Typography variant="body2" color="text.secondary">
-							Birds are very intriguing creatures as they can range from birds as small as a Cuban bee hummingbird (about two inches long and a little over 1/20 of an ounce) to as large as an ostrich. Click learn more below to find out more about caring for your bird.
+							Birds are very intriguing creatures as they can range from birds as small as a Cuban bee hummingbird (about two inches
+							long and a little over 1/20 of an ounce) to as large as an ostrich. Click learn more below to find out more about caring
+							for your bird.
 						</Typography>
 					</CardContent>
 					<CardActions>
 						<Button size="small">Share</Button>
-						<Button size="small"><a href='views/birds'>Learn More</a></Button>
+						<Button size="small">
+							<Link to="/views/birds">Learn More</Link> {/* Use Link component */}
+						</Button>
 					</CardActions>
 				</Card>
 			</CardContainer>
 		</>
-  );
+	);
 }
